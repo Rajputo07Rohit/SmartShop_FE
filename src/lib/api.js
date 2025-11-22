a272b42573a8e17c9c78d202feb4496612ec06d1
Rajputo07Rohit <rajputrohit2kk@gmail.com>
2025-11-22 17:39:26 +0530

    local host is implemenet

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const API_BASE =
-  import.meta.env.VITE_API_BASE ||
-  // "http://localhost:4000" ||
-  "https://api-smartstore.onrender.com";
+// const API_BASE =
+//   import.meta.env.VITE_API_BASE ||
+//   // "http://localhost:4000" ||
+//   "https://api-smartstore.onrender.com";
+
+const API_BASE = "http://localhost:4000";
 
 export const api = axios.create({
   baseURL: `${API_BASE}/api`,
